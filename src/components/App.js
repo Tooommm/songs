ed8fa9a8b05e00a98b5ecf751e2bc259451769e5
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-12-09 17:23:57 +0100

    en cours

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import SongList from './SongList';
 import { selectSong } from '../actions';
 
 const App = () => {
-  return <h1>App</h1>
-}
+  return (
+  <div className="ui container grid">
+    <div className="ui row">
+      <div className="column eight wide">
+        <SongList />
+      </div>
+    </div>
+  </div>
+  );
+};
 
 export default App;
