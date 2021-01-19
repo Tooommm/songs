b6a672b5a4ce9fdac5d925662f2802b04bb99063
Thomas Brouillet <thomasbrouillet@gmail.com>
2021-01-19 14:17:10 +0100

    completed exercise

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -8,8 +8,11 @@ class SongList extends React.Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
-              Select
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+              >
+                Select
             </button>
           </div>
           <div className="content">{song.title}</div>
@@ -26,4 +29,7 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,6 @@ const songsReducer = () => {
     { title: 'No Scrubs', duration: '4:05' },
     { title: 'Macarena', duration: '2:30' },
     { title: 'All Star', duration: '3:15' },
-    { title: 'All Star', duration: '3:15' },
     { title: 'I Want it That Way', duration: '1:45' },
   ];
 };
